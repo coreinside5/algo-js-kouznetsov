50f3837411167068502bc7abf2168083bec4cff3
Maxim K <90226452+coreinside5@users.noreply.github.com>
2021-09-23 16:26:24 +0200

    commit2

---
+++ 2_5.js
@@ -0,0 +1,8 @@
+const readlineSync = require("readline-sync");
+
+
+let magicNumber =parseInt(readlineSync.question('Enter your favorite number'));
+while
+if (magicNumber != 42){
+	console.log('Are you sure?');
+}
