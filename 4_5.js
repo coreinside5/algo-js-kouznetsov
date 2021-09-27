fb662c0d547f916b8d71ba152bd15a4a79879346
Maxim K <90226452+coreinside5@users.noreply.github.com>
2021-09-27 12:09:46 +0200

    4_5

---
+++ 4_5.js
@@ -0,0 +1,22 @@
+const readlineSync = require("readline-sync");
+/*calcDistance takes two arguments pt1 and pt2 which are 2 small arrays with [x,y] values respectively
+then it calculate distance between two points and return the value of this calculation*/
+function calcDistance (pt1,pt2)
+{
+	let dist = 0;
+	dist = Math.sqrt(Math.pow((pt2Arr [1] - pt1Arr [1]), 2)+Math.pow((pt2Arr [0] - pt1Arr[0] ), 2));	
+	return dist;
+}
+let pt1Arr = [];
+let pt2Arr = [];
+
+console.clear();
+console.log("Hello!");
+console.log("Ejercicio 4.5");
+pt1Arr[0] = parseInt(readlineSync.question("Entrex la coordonnee X pour le 1er point :"));
+pt1Arr [1]= parseInt(readlineSync.question("Entrex la coordonnee Y pour le 1er point :"));
+pt2Arr [0]= parseInt(readlineSync.question("Entrex la coordonnee X pour le 2eme point :"));
+pt2Arr [1]= parseInt(readlineSync.question("Entrex la coordonnee Y pour le 2eme point :"));
+console.log(pt1Arr);
+console.log(pt2Arr);
+console.log(calcDistance(pt1Arr,pt2Arr));
